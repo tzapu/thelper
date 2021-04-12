58adb8cda8c06e4480abb84acf733aedc1412cb0
Alex T <alex+page@tzapu.com>
2021-04-12 13:32:16 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tzapu/thelper
+
+go 1.16
---
+++ thelper.go
@@ -0,0 +1,57 @@
+package thelper
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func Load(t *testing.T, fn string, out interface{}) {
+	t.Helper()
+
+	raw := LoadFile(t, fn)
+
+	err := json.Unmarshal(raw, out)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Save(t *testing.T, fn string, out interface{}) {
+	t.Helper()
+	b, err := json.MarshalIndent(out, "", "  ")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ioutil.WriteFile(fn, b, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func LoadFile(t *testing.T, fn string) []byte {
+	t.Helper()
+
+	raw, err := ioutil.ReadFile(fn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return raw
+}
+
+func SaveOnUpdate(t *testing.T, u *bool, fn string, g interface{}) {
+	t.Helper()
+	if *u {
+		Save(t, fn, g)
+	}
+}
+
+// SkipCI skips tests of the CI env var is set
+func SkipCI(t *testing.T) {
+	t.Helper()
+	if os.Getenv("CI") != "" {
+		t.Skip("Skipping testing in CI environment")
+	}
+}
